e311763cae02b5315e7066f16ef15141a2ab95b9
adriamdevs <murigucharles3@gmail.com>
2025-10-09 15:07:38 +0300

    mod: add swiper lib & modify hero carousel

--- src/components/home/HeroSect.vue
+++ src/components/home/HeroSect.vue
@@ -1,9 +1,9 @@
 <template>
+    <!--Carousel Items-->
     <div class="relative block w-full">
         <img src="/src/assets/images/gallery-1497894743-gettyimages-582802922.jpg" class="inset-0" alt="">
         <div class="absolute text-2xl text-white bottom-4">
             <p class="text-wrap">Developing Robust and advanced solutions to commonplace problems</p>
         </div>
-
     </div>
 </template>
