875682e4da542a6463d74fda214f16820e5791d9
adriamdevs <murigucharles3@gmail.com>
2025-10-11 10:49:00 +0300

    mod: fix not found route reference

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>404</h1>
+        <p>OOPS! The page you are looking for cannot be found</p>
+        <RouterLink to="/"> Homepage</RouterLink>
+        <RouterView/>
+    </div>
+</template>
