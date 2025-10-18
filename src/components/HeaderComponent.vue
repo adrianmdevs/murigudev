c567ebf0946d863ad373af98c595726ae2d415db
adriamdevs <murigucharles3@gmail.com>
2025-10-18 11:15:26 +0300

    mod(ui): modify header styling

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -17,7 +17,7 @@ onUnmounted(()=>{
 })
 </script>
 <template>
-    <header class="w-full fixed top-0 z-[999] px-6 py-4 border-b bg-gray-200 border-gray-500/20 dark:border-gray-500/50 shadow-sm md:rounded-lg z-99 max-sm:px-3 dark:bg-black md:border-gray-300">
+    <header class="w-full px-6 py-4 bg-gray-200 border-b shadow-sm border-gray-500/20 dark:border-gray-500/50 md:rounded-lg z-99 max-sm:px-3 dark:bg-black md:border-gray-300">
         <div class="flex flex-row justify-between w-full lg:container lg:mx-auto">
         <img src="../assets/images/murigu.png" class="w-1/3 h-auto lg:w-1/4" alt="">
         <button @click="toggleDark" class="text-xl focus:outline-none pe-1.5">
